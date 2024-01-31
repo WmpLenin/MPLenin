7c939f3e687fe431aeb840cdb427add259d271af
MPLenin <ggutierrescaceres@gmail.com>
2024-01-31 01:07:10 -0500

    First commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = User::create([
+            'name' => 'admin',
+            'email' => 'admin@lenin.net',
+            'password' => Hash::make('admin')
+        ]);
+        $admin->assignRole('admin');
+
+        $editor = User::create([
+            'name' => 'editor',
+            'email' => 'editor@lenin.net',
+            'password' => Hash::make('editor')
+        ]);
+        $editor->assignRole('editor');
+
+    }
+}
---
+++ resources/js/Components/Welcome.vue
@@ -0,0 +1,7 @@
+<script setup>
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+</script>
+
+<template>
+    <h1>Hello world</h1>
+</template>
